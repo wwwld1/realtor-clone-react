04dfab1be4eb40708226418bc374b33a4e153c46
Leo Wang <lw3062@nyu.edu>
2023-10-03 22:58:04 -0400

    complete the header component and make it dynamic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
 
 export default function Header() {
+  const [pageState, setPageState] = useState('Sign in')
   const location = useLocation()
   const navigate = useNavigate()
-  console.log(location.pathname);
+  const auth = getAuth()
+  useEffect(() => {
+    onAuthStateChanged(auth, user => {
+      if (user) {
+        setPageState('Profile')
+      }
+      else {
+        setPageState('Sign in')
+      }
+    })
+  })
   function pathMatchRoute(route) {
     return route === location.pathname
   }
@@ -18,7 +30,12 @@ export default function Header() {
           <ul className='flex space-x-10'>
             <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent${pathMatchRoute('/') && 'text-black border-b-red-500'}`} onClick={() => navigate('/')}>Home</li>
             <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent${pathMatchRoute('/offers') && 'text-black border-b-red-500'}`} onClick={() => navigate('/offers')}>Offers</li>
-            <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent${pathMatchRoute('/sign-in') && 'text-black border-b-red-500'}`} onClick={() => navigate('/sign-in')}>Sign in</li>
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent${(pathMatchRoute('/sign-in') || pathMatchRoute('/profile')) && 'text-black border-b-red-500'}`}
+              onClick={() => navigate('/profile')}
+            >
+              {pageState}
+            </li>
           </ul>
         </div>
       </header>
