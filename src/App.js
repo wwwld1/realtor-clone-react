8f88e054c74f62e90627ddeef94657e8feda3073
LeoWang <lw3062@nyu.edu>
2023-11-30 22:46:17 -0500

    add functionality to create listing page

--- src/App.js
+++ src/App.js
@@ -23,7 +23,9 @@ function App() {
           <Route path="/offers" element={<Offers />} />
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/create-listing" element={<CreateListing />} />
+          <Route path="/create-listing" element={<PrivateRoute />}>
+            <Route path="/create-listing" element={<CreateListing />} />
+          </Route>
           <Route path="/forgot-password" element={<ForgotPassword />} />
         </Routes>
       </Router>
